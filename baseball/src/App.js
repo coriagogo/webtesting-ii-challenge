fc7f6f2eac9bcf6d04fa989ede926ab56dae2a63
coriagogo <pkcparis@gmail.com>
2019-06-18 15:25:36 -0400

    control buttons added

--- baseball/src/App.js
+++ baseball/src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    balls: 0,
+    strikes: 0,
+    fouls: 0,
+  };
+
+  render() {
+    return(
+      <div className="App">
+        <div>
+          <div>Balls: {this.state.balls}</div>
+          <div>Strikes: {this.state.strikes}</div>
+        </div>
+        <div className="controls">
+          <button type="button" onClick={this.strike}>Strike</button>
+          <button type="button" onClick={this.ball}>Ball</button>
+          <button type="button" onClick={this.foul}>Foul</button>
+          <button type="button" onClick={this.hit}>Hit</button>
+        </div>
+      </div>
+    )
+  }
+
+
 }
 
 export default App;
